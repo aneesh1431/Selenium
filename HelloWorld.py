0348096bc24779a5457ae50e22ef37bd59d97bce
aneesh1431 <36808239+aneesh1431@users.noreply.github.com>
2018-10-02 22:08:41 +0530

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+print ("I am awesome!!!!!!!!!!!")
+num1 = 3
+num2 = 2
+
+print("Multiplication:", num1 ** num2,"is the answer")
+print("Modulus :", num1 % num2)
+print("Division :"+ str( num1 / num2))
+print("Roundoff Division :"+ str( num1 // num2))
